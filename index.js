722b33ffadeb1e4e11a14bed08c62a81f471d3ae
ryip94 <ryip94@yahoo.com>
2023-05-10 20:31:15 -0700

    Modified functions so that you cannot utilize "HIT ME" if the game is already over.
    Also modified "START GAME" so that you cannot
    start a new game while in the middle of one

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let allCards = [firstCard, secondCard]
-let sum = firstCard + secondCard;
+let allCards = []
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let outcome = document.getElementById("result");
 let message = "";
 let sumEl = document.getElementById("sum-el");
@@ -28,6 +26,7 @@ function renderGame() {
     } else if (sum === 21) {
         //outcome.innerText = "Hooray! Blackjack!";
         hasBlackJack = true;
+        isAlive = false;
         message = "Hooray! Blackjack!";
     } else {
         //outcome.innerText = "You lose";
@@ -39,6 +38,9 @@ function renderGame() {
 }
 
 function newCard() {
+    if (isAlive === false) {
+        return
+    } 
     let drawCard = getRandomCard();
     sum += drawCard;
     allCards.push(drawCard);
@@ -46,6 +48,14 @@ function newCard() {
 }
 
 function startGame() {
+    if (isAlive === true) {
+        return
+    } 
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    allCards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame();
 }
 
