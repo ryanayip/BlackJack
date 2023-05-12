238ffdd288c34cc06834adcbf13e039ddcae39da
ryip94 <ryip94@yahoo.com>
2023-05-12 16:27:55 -0700

    Fixed logic that makes aces worth either 1 or 11 depending on the total of the hand.

---
+++ cardlogic.js
@@ -0,0 +1,134 @@
+let allCards = []
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let outcome = document.getElementById("result");
+let message = "";
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+const cardArea = document.getElementById("card-area");
+//card image tie ins
+//let twoClubs = document.getElementById("cardImageId");
+//twoClubs.src = "/sources/cards/2_of_clubs.png";
+
+
+
+
+//player object
+let player = {
+    name: "Your Chips",
+    chips: 1000
+}
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
+
+
+
+//Working render game function, but always keeps the Ace at 11
+function renderGame() {
+    let aceCount = 0;
+    sum = 0;
+    let cardsHTML = "";
+    for (let i = 0; i < allCards.length; i++) {
+        let cardValue = allCards[i];
+        if (cardValue > 11) {
+            cardValue = 10;
+        }
+        if (cardValue === 11) {
+            aceCount++;
+            cardValue = 1;
+        }
+        sum += cardValue;
+        cardsHTML += "<img class='cardimage' src='/sources/cards/" + cardValue + "_of_" + getSuit(i) + ".png'>";
+    }
+    // Handle ace cards
+    while (aceCount > 0 && sum + 10 <= 21) {
+        sum += 10;
+        aceCount--;
+    }
+    sumEl.innerText = "Sum: " + sum;
+
+    // Win or Lose logic
+    if (sum < 21) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        hasBlackJack = true;
+        isAlive = false;
+        message = "Hooray! Blackjack!";
+    } else {
+        isAlive = false;
+        message = "You lose";
+    }
+    outcome.innerText = message;
+
+    // Display cards
+    cardArea.innerHTML = cardsHTML;
+}
+
+function getSuit(index) {
+    const suits = ["clubs", "diamonds", "hearts", "spades"];
+    return suits[index % 4];
+}
+
+
+function newCard() {
+    if (isAlive === true) {
+        let drawCard = getRandomCard();
+        sum += drawCard;
+        allCards.push(drawCard);
+        renderGame();
+    } 
+}
+
+
+
+function startGame() {
+    if (isAlive === false) {
+        isAlive = true;
+        cardArea.innerHTML = "";
+        let firstCard = getRandomCard();
+        let secondCard = getRandomCard();
+        allCards = [firstCard, secondCard]
+        if (firstCard > 11) {
+            firstCard = 10
+        }
+
+        if (secondCard > 11) {
+            secondCard = 10
+        }
+
+        sum = firstCard + secondCard
+
+        // Remove all card images before starting new game
+        let cardImages = document.querySelectorAll(".cardimage");
+        for (let i = 0; i < cardImages.length; i++) {
+            cardImages[i].remove();
+        }
+
+        renderGame();
+    } 
+}
+
+    
+
+
+function getRandomCard() {
+
+
+    let randomNumber = Math.floor(Math.random() * 10) + 2;
+    return randomNumber
+}
+
+//return a random number between 12-14 to assign a face card instead of always a "10"
+function getFaceCard() {
+    let randomNumber = Math.floor(Math.random() * 3) + 12;
+    return randomNumber
+}
+
+
+
+// function randomSuit() {
+//     let randomSuit = Math.floor(Math.random() * 4) + 1;
+//     return randomSuit
+// }
--- index.js
+++ index.js
@@ -12,23 +12,6 @@ const cardArea = document.getElementById("card-area");
 //twoClubs.src = "/sources/cards/2_of_clubs.png";
 
 
-//Random card generator with image
-function addRandomCard() {
-    //create a variable that will generate a random number between 1-13
-    let randomNumber = Math.floor(Math.random() * 13) + 1;
-    //create a variable that will generate a random suit
-    let randomSuit = Math.floor(Math.random() * 4) + 1;
-    //create a variable that will store the image url
-    let cardImage = "/sources/cards/" + randomNumber + "_of_" + randomSuit + ".png";
-    //create a variable that will create an image element
-    const newCardPic = document.createElement("img");
-    //add a class to the image element
-    newCardPic.setAttribute("class","cardimage")
-    //add the image url to the image element
-    newCardPic.src = cardImage;
-    //add the image element to the cardArea
-    cardArea.appendChild(newCardPic);
-}
 
 
 //player object
@@ -42,7 +25,7 @@ playerEl.textContent = player.name + ": $" + player.chips;
 
 
 
-
+//Working render game function, but always keeps the Ace at 11
 function renderGame() {
 
     
@@ -52,13 +35,28 @@ function renderGame() {
        // cardsEl.textContent += allCards[i] + " "
 
         let cardvalue = allCards[i];
-        //to include face cards when the value is 10
-        if (cardvalue == 10) {
+        //to include face cards when the value 
+        if (cardvalue > 11) {
             cardvalue = getFaceCard();
         }
 
+        if (cardvalue === 11) {
+            if (sum + 11 <= 21) {
+                cardvalue = 11;
+            } else {
+                cardvalue = 1;
+            }
+        }    
+
         //value of newly added card
         let newestCard = parseInt(allCards[allCards.length - 1]);
+
+        if (cardvalue === 11) {
+            if (sum + 11 > 21) {
+                cardvalue = 1;
+            }
+        }
+        
 
         if (allCards.length > 2) {
             //only display the newest card in the array one single time
@@ -100,21 +98,7 @@ function renderGame() {
             }
             outcome.innerText = message;
             
-        
 
-        // This logic below works, but incorrectly displays the newest card too many times
-        // if (allCards.length > 2) {
-        //     let newCardPic = document.createElement("img");
-        //     newCardPic.src = "/sources/cards/" + newestCard + "_of_" + randomSuit() + ".png";
-        //     newCardPic.setAttribute("class","cardimage")
-        //     cardArea.appendChild(newCardPic);
-        // } else {
-        //     let newCardPic = document.createElement("img");
-        //     newCardPic.src = "/sources/cards/" + cardvalue + "_of_" + randomSuit() + ".png";
-        //     newCardPic.setAttribute("class","cardimage")
-        //     cardArea.appendChild(newCardPic);
-        
-    }
 
     //Win or Lose logic
     sumEl.innerText = "Sum: " + sum;
@@ -130,8 +114,7 @@ function renderGame() {
     }
     outcome.innerText = message;
    
-} }
-
+} } }
 
 function newCard() {
     if (isAlive === true) {
@@ -143,6 +126,7 @@ function newCard() {
 }
 
 
+
 function startGame() {
     if (isAlive === false) {
         isAlive = true;
@@ -151,18 +135,16 @@ function startGame() {
         let secondCard = getRandomCard();
         let displayCards = [firstCard, secondCard]
         allCards = [firstCard, secondCard]
-        if (firstCard > 10) {
+        if (firstCard > 11) {
             firstCard = 10
         }
 
-        if (secondCard > 10) {
+        if (secondCard > 11) {
             secondCard = 10
         }
 
         sum = firstCard + secondCard
         renderGame();
-        //addRandomCard();
-
     } 
 }
 
@@ -171,27 +153,20 @@ function startGame() {
 
 function getRandomCard() {
 
-    //return a random number between 1-10
 
-    let randomNumber = Math.floor(Math.random() * 10) + 1;
+    let randomNumber = Math.floor(Math.random() * 10) + 2;
     return randomNumber
-    // if (randomNumber === 1) {
-    //     return 11 }
-    // else if (randomNumber > 10) {
-    //         return 10
-    // } else {
-    //     return randomNumber
-    // }
 }
 
-//return a random number between 10-13 to assign a face card instead of always a "10"
+//return a random number between 12-14 to assign a face card instead of always a "10"
 function getFaceCard() {
-    let randomNumber = Math.floor(Math.random() * 4) + 10;
+    let randomNumber = Math.floor(Math.random() * 3) + 12;
     return randomNumber
 }
+
+
 
 function randomSuit() {
     let randomSuit = Math.floor(Math.random() * 4) + 1;
     return randomSuit
-
 }
